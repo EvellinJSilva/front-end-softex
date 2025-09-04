b938c76f6404cda74d8cc50d44e8e0e45ddb88b9
Évellin Silva <evellin.j.silva@gmail.com>
2025-09-03 22:53:47 -0300

    Parte 2 - lista de exercicios 1

--- Atv Contar Pares e Impares/script.js
+++ Atv Contar Pares e Impares/script.js
@@ -1,8 +1,24 @@
-function gerarNumeros() {
-  const numeros = Array.from({ lenght: 10 }, () => Math.floor(Math.random() * 100) + 1);
-  const pares = numeros.filter((n) => n % 2 === 0).length;
+document.addEventListener("DOMContentLoaded", () => {
+
+  function gerarNumeros() {
+  //const numeros = Array.from({ lenght: 10 }, () => Math.floor(Math.random() * 100) + 1);
+
+  let numeros = [];
+
+  for (let i = 0; i < 10; i++) {
+    numeros[i]  = Math.floor(Math.random() * 100) + 1;
+  }
+
+  //const pares = numeros.filter((n) => n % 2 === 0).length;
+  let pares = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+      pares = pares + 1;
+    }
+  }
   const impares = numeros.length - pares;
-  const numerosEl = document.getElementById("numeros");
+  const numerosEl = document.getElementById("numerosSorteados");
   const paresEl = document.getElementById("pares");
   const imparesEl = document.getElementById("impares");
 
@@ -10,8 +26,5 @@ function gerarNumeros() {
   paresEl.textContent = `Quantidade de pares: ${pares}`;
   imparesEl.textContent = `Quantidade de impares: ${impares}`;
 }
-
-document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("btn").addEventListener("click", gerarNumeros);
-  gerarNumeros();
 });
--- Atv Maior e Menor Valor/script.js
+++ Atv Maior e Menor Valor/script.js
@@ -4,19 +4,44 @@ document.addEventListener("DOMContentLoaded", () => {
   const menorEl = document.getElementById("menor");
 
   btnComparar.addEventListener("click", () => {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let num3 = Number(document.getElementById("num3").value);
-    let num4 = Number(document.getElementById("num4").value);
-    let num5 = Number(document.getElementById("num5").value);
+    //let num1 = Number(document.getElementById("num1").value);
+    //let num2 = Number(document.getElementById("num2").value);
+    //let num3 = Number(document.getElementById("num3").value);
+    //let num4 = Number(document.getElementById("num4").value);
+    //let num5 = Number(document.getElementById("num5").value);
 
-    if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4) || isNaN(num5)) {
-      alert("Por favor, insira números válidos!");
-      return;
+    //if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4) || isNaN(num5)) {
+      //alert("Por favor, insira números válidos!");
+      //return;
+    //}
+
+    //let maior = Math.max(num1, num2, num3, num4, num5);
+    //let menor = Math.min(num1, num2, num3, num4, num5);
+
+    let maior = 0;
+    let menor = 0;
+    let elementos = [];
+
+    for (let i = 0; i < 5; i++) {
+       elementos[i] = Number(document.getElementById(`num${i + 1}`).value);
     }
 
-    let maior = Math.max(num1, num2, num3, num4, num5);
-    let menor = Math.min(num1, num2, num3, num4, num5);
+    for (let i = 0; i < elementos.length; i++) {
+      if (isNaN(elementos[i])) {
+        alert("Por favor, insira números válidos!");
+        return;
+      }
+      //if (i == 0) {
+       // maior = elementos[i];
+        //menor = elementos[i];
+      //} else {
+       // maior = Math.max(maior, elementos[i]);
+       // menor = Math.min(menor, elementos[i]);
+      //}
+    }
+
+    maior = Math.max(...elementos);
+    menor = Math.min(...elementos);
 
     maiorEl.innerHTML = `Maior número: <span class="negrito">${maior}</span>`;
     menorEl.innerHTML = `Menor número: <span class="negrito">${menor}</span>`;
